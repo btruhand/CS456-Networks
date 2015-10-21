0a98ec4c90c2d24213f6a94b837952e1d1231875
Btara Truhandarien <btara.truhandarien@gmail.com>
2015-10-21 03:19:55 -0400

    Made only one UDP socket for the server

--- A1/server.py
+++ A1/server.py
@@ -6,46 +6,50 @@ from udp import udpsender, udpreceiver
 BUFSIZE = 1024
 
 def server(reqcode):
-#retrieve the IP address of server's machine
-	hostname = socket.gethostbyname(socket.gethostname())
+    #retrieve the IP address of server's machine
+    hostname = socket.gethostbyname(socket.gethostname())
+    
+    serverSocket = getsocket(socket.SOCK_STREAM)
+    print 'SERVER_PORT=' + str(serverSocket.getsockname()[1])
+    
+    #start listening, queue 0 connections because we only have one client at a time
+    serverSocket.listen(0)
+    
+    # create a UDP socket
+    serverUDP = getsocket(socket.SOCK_DGRAM)
+    r_port = str(serverUDP.getsockname()[1])
 
-	serverSocket = getsocket(socket.SOCK_STREAM)
-	print 'SERVER_PORT=' + str(serverSocket.getsockname()[1])
+    while True:
+        sys.stderr.write('Accepting Connection...\n')
+        conn, addr = serverSocket.accept()
 
-#start listening, queue 0 connections because we only have one client at a time
-	serverSocket.listen(0)
+        # accept a TCP connection from the client
+        # conn is the socket endpoint of the client and addr is the address bound to conn
+        # receive 32 bytes, enough size for an integer
+        data = conn.recv(32)
+        
+        if reqcode != data:
+            # received incorrect req_code from client, close TCP connection with client
+            conn.shutdown(1)
+            continue
+        
+        # send the port number bound to the UDP socket over to the client
+        conn.sendall(r_port)
+       
+        # receive the message from client
+        sys.stderr.write('Waiting to receive message...\n')
+        message, clientaddr = udpreceiver(serverUDP, BUFSIZE)
 
-	while True:
-		sys.stderr.write('Accepting Connection...\n')
-		conn, addr = serverSocket.accept()
+        #reverse message
+        message = message[::-1]
 
-		# accept a TCP connection from the client
-		# conn is the socket endpoint of the client and addr is the address bound to conn
-		# receive 32 bytes, enough size for an integer
-		data = conn.recv(32)
-		
-		if reqcode != data:
-			# received incorrect req_code from client, close TCP connection with client
-			conn.shutdown(1)
-			continue
-		
-		# create a UDP socket
-		serverUDP = getsocket(socket.SOCK_DGRAM)
+        sys.stderr.write('Sending reversed message to client...\n')
+        udpsender(serverUDP, message, clientaddr)
 
-		# send the port number bound to the UDP socket over to the client
-		conn.sendall(str(serverUDP.getsockname()[1]))
-	   
-		# receive the message from client
-		sys.stderr.write('Waiting to receive message...\n')
-		message, clientaddr = udpreceiver(serverUDP, BUFSIZE)
-
-		#reverse message
-		message = message[::-1]
-
-		sys.stderr.write('Sending reversed message to client...\n')
-		udpsender(serverUDP, message, clientaddr)
-		# close UDP socket
-		serverUDP.close()
+    # close UDP socket
+    serverUDP.close()
+    # close TCP connection
+    serverSocket.close()
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
