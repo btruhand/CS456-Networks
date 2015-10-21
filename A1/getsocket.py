4c5b4256418e9be7580206bd87a0f632bb0260fd
Btara Truhandarien <btara.truhandarien@gmail.com>
2015-10-21 00:49:59 -0400

    Fixed try and catch

--- A1/getsocket.py
+++ A1/getsocket.py
@@ -6,30 +6,26 @@ def getsocket(sockettype):
 	# use all the IP address of this machine
 	# including localhost (127.0.0.1) equivalent
 	# to socket.INADDR_ANY
-	hostname = ''
-
-	s = None
-	port = None
-	# attempt to make a socket
-	while True:
-		portSocket = socket.socket(socket.AF_INET, sockettype)
-		# request a port
-		portSocket.bind((hostname, 0))
-		port = portSocket.getsockname()[1]
-		portSocket.close()
-
-		try:
-			s = socket.socket(socket.AF_INET, sockettype)
-			#attempt to bind to port
-			try:
-				s.bind((hostname, port))
-			except socket.error:
-				#failure to bind
-				continue
-		except socket.error:
-			#failure to create socket
-			continue
-		else:
-			break
-	
-	return s
+    hostname = ''
+    
+    s = None
+    port = None
+    # attempt to make a socket
+    while True:
+        try:
+            portSocket = socket.socket(socket.AF_INET, sockettype)
+            # request a port
+            portSocket.bind((hostname, 0))
+            port = portSocket.getsockname()[1]
+            portSocket.close()
+            
+            # attempt to get socket
+            s = socket.socket(socket.AF_INET, sockettype)
+            # attempt to bind to port
+            s.bind((hostname, port))
+            # managed to get a socket connection
+            break
+        except socket.error:
+            # failure to create socket or bind to socket
+            continue
+    return s
